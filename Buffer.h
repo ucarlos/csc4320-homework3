ee160397afc60d5a731547b5ae7b4ce5f0152dec
Ulysses Carlos <ulycarlos@gmail.com>
2020-09-28 23:11:01 -0400

    Wrote the initial error handling for main

--- Buffer.h
+++ Buffer.h
@@ -1,7 +1,12 @@
 /* buffer.h*/
+#ifndef BUFFER_H
+#define BUFFER_H
 
+#define BUFFER_SIZE (5)
+// Struct Declaration
 typedef int buffer_item;
-#define BUFFER_SIZE 5
 
+// Struct Definition
 int insert_item(buffer_item item);
 int remove_item(buffer_item *item);
+#endif
--- Homework3.c
+++ Homework3.c
@@ -2,13 +2,15 @@
  * -----------------------------------------------------------------------------
  * Created by Ulysses Carlos on 09/28/2020 at 09:00 PM
  * 
- * hw3.c
+ * Homework3.c
  * 
  * -----------------------------------------------------------------------------
  */
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
+#include <ctype.h>
 #include <pthread.h>
 #include <semaphore.h>
 #include "Buffer.h"
@@ -20,36 +22,46 @@ buffer_item buffer[BUFFER_SIZE];
 void *producer(void *param);
 void *consumer(void *param);
 
+//------------------------------------------------------------------------------
+// Buffer_item function definitions
+//------------------------------------------------------------------------------
 int insert_item(buffer_item item){
-
     	/* Acquire Empty Semaphore */
-		
 	/* Acquire mutex lock to protect buffer */
-	
 	/* Insert item into buffer */
-	
-
 	/* Release mutex lock and full semaphore */
-	
-
 	return 0;
 }
 
 int remove_item(buffer_item *item){
-
-	/* Acquire Full Semaphore */
-	
-
-	/* Acquire mutex lock to protect buffer */
-	
-	/* remove an object from buffer placing it in item */
-	
-
-	/* Release mutex lock and empty semaphore */
+    /* Acquire Full Semaphore */	
+    /* Acquire mutex lock to protect buffer */
+    /* remove an object from buffer placing it in item */
+    /* Release mutex lock and empty semaphore */
 	
 	return 0;
 }
 
+
+void* producer(void *param){
+
+    /* producer thread that calls insert_item() */
+    return NULL;
+}
+
+void* consumer(void *param){
+
+    /* consumer thread that calls remove_item() */
+    return NULL;
+}
+
+bool str_isdigit(char *string){
+    for (char *p = string; *p; p++)
+	if (!isdigit(*p)) return false;
+    return true;
+}
+
+//------------------------------------------------------------------------------
 int main(int argc, char *argv[]){
     /* Get command line arguments argv[1],argv[2],argv[3] */
     /* Initialize buffer related synchronization tools */
@@ -57,18 +69,35 @@ int main(int argc, char *argv[]){
     /* 	    Create consumer threads based on the command line input */
     /* 	    Sleep for user specified time based on the command line input */
 	
-	
-	return 0;
-}
+    if (argc != 4){
+	fprintf(stderr, "Usage: ./Homework3 [Sleep Time (s)] "
+		"[# of Producer Threads] [# of Consumer Threads]\n");
+	exit(EXIT_FAILURE);
+    }
+    
+    // Prevent invalid input for arguments:
+    bool check = str_isdigit(argv[1]) && str_isdigit(argv[2])
+	&& str_isdigit(argv[3]);
+    
+    if (!check){
+	fprintf(stderr, "Error: Argument(s) contain nonnumerical characters.\n");
+	exit(EXIT_FAILURE);
+    }
 
-void* producer(void *param){
+    int sleep_time = atoi(argv[1]);
+    int producer_num = atoi(argv[2]);
+    int consumer_num = atoi(argv[3]);
 
-	/* producer thread that calls insert_item() */
-    return NULL;
-}
+    if (sleep_time < 0){
+	fprintf(stderr, "Error: Cannot have a negative sleep time.\n");
+	exit(EXIT_FAILURE);
+    }
 
-void* consumer(void *param){
+    if (producer_num < 1 || consumer_num < 1){
+	fprintf(stderr, "Error: There must be at least one producer and consumer"
+		" thread.\n");
+	exit(EXIT_FAILURE);
+    }
 
-	/* consumer thread that calls remove_item() */
-    return NULL;
+    // Now do the stuff
 }
